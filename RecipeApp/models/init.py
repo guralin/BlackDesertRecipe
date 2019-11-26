38e42cb26fd6b68ee3197dae9d1118e6be34ef61
guralin <tellurium38@gmail.com>
2019-11-26 09:58:38 +0900

    csvからデータベースのテーブルをすぐ作成できるようモジュールを追加

--- RecipeApp/models/init.py
+++ RecipeApp/models/init.py
@@ -10,43 +10,33 @@ from setting import ENGINE
 class Item(Base):
     __tablename__ = 'item'
 
-    p_id = Column('id', Integer, Sequence('item_id_seq'), primary_key=True)
-    name = Column('name', String(128), nullable=False)
+    item_id = Column('item_id', Integer, Sequence('item_id_seq'), primary_key=True)
+    name = Column('name', String(128), nullable=False, unique=True)
     detail = Column('detail', String(512))
     NPC_price = Column('NPC_price', Integer)
     exchange_price = Column('exchange_price', Integer)
 
     def __repr__(self):
-        #TODO:f""形式でできるとわかったら必ずリファクタリングする
-        return "<Item(p_id='%s', name='%s', detail='%s' NPC_price='%s' exchange_price='%s')>" %(self.p_id, self.name, self.detail, self.NPC_price, self.exchange_price)
+        return f"<Item(item_id={self.item_id}, name={self.name}, detail={self.detail}, NPC_price{self.NPC_price}, exchange_price={self.exchange_price})>"
+        
 
 
 class Recipe(Base):
     __tablename__ = 'recipe'
 
     recipe_id = Column('recipe_id', Integer, Sequence('recipe_id_seq'), primary_key=True)
-    category_id = Column('category', Integer)
-    finished_item = Column('finished_item', Integer)
-    material_1_id = Column('material_1_id', Integer)
-    material_2_id = Column('material_2_id', Integer)
-    material_3_id = Column('material_3_id', Integer)
-    material_4_id = Column('material_4_id', Integer)
-    material_5_id = Column('material_5_id', Integer)
-    material_6_id = Column('material_6_id', Integer)
-    material_1_count = Column('material_1_count', Integer)
-    material_2_count = Column('material_2_count', Integer)
-    material_3_count = Column('material_3_count', Integer)
-    material_4_count = Column('material_4_count', Integer)
-    material_5_count = Column('material_5_count', Integer)
-    material_6_count = Column('material_6_count', Integer)
+    category_id = Column('category_id', Integer, nullable=False)
+    finished_item_id = Column('finished_item_id', Integer,nullable=False)
+    material_id = Column('material_id', Integer,nullable=False)
+    material_count = Column('material_count', Integer,nullable=False)
 
     def __repr__(self):
-        return f"<Recipe(recipe_id={self.recipe_id}, category_id={self.category_id}, finished_item={self.finished_item}, material_1_id={self.material_1_id}, material_2_id={self.material_2_id}, material_3_id={self.material_3_id}, material_4_id={self.material_4_id}, material_5_id={self.material_5_id}, material_6_id={self.material_6_id})>"
+        return f"<Recipe(recipe_id={self.recipe_id}, category_id={self.category_id}, finished_item_id={self.finished_item_id}, material_id={self.material_id}, material_count={self.material_count})>"
 
 class Category(Base):
     __tablename__ = "category"
     category_id = Column('category_id', Integer, primary_key=True)
-    category_name = Column('category_name', String(128))
+    category_name = Column('category_name', String(128), nullable=False)
 
     def __repr__(self):
         return f"<Category(category_id={self.category_id}, category_name={self.category_name})>"
--- RecipeApp/models/recipe.py
+++ RecipeApp/models/recipe.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setting import session
+import csv
 
-from init import Recipe, Item
+from setting import session
+from init import Recipe, Item, Category
 
 def add_item(item_name, detail="", NPC_price=0, exchange_price=0):
     item = Item()
@@ -20,3 +21,61 @@ def show_item():
     items = session.query(Item).all()
     for item in items:
         print(item)
+
+def item_name_to_id(name):
+    match_list = session.query(Item).filter(Item.name==name).all()
+    if len(match_list) == 1:
+        return match_list[0].item_id
+    else:
+        raise ValueError("該当するidが存在しません")
+
+def id_to_item_name(item_id):
+    match_list = session.query(Item).filter(Item.item_id==item_id).all()
+    if len(match_list) == 1:
+        return match_list[0].name
+    else:
+        raise ValueError("該当するidが存在しません")
+
+def add_item_when_dont_exist(name):
+    match_count = session.query(Item).filter(Item.name==name).count() 
+    if match_count == 0:
+        add_item(name)
+
+def import_item_for_csv(path):
+    with open(path,"r",encoding="shift-jis") as f:
+        cin = csv.reader(f)
+        villains = [row for row in cin]
+        for row in villains:
+            add_item_when_dont_exist(row[2])
+            add_item_when_dont_exist(row[3])
+
+def category_name_to_id(category_name):
+    match_list = session.query(Category).filter(Category.category_name==category_name).all()
+    if len(match_list) == 1:
+        return match_list[0].category_id
+    else:
+        raise ValueError("該当するカテゴリーが存在しません")
+
+def add_category(category_name):
+    category = Category()
+    category.category_name = category_name
+    session.add(category)
+    session.commit()
+
+def add_recipe(category, finished_item, material, material_count):
+    recipe = Recipe()
+    recipe.category_id = category_name_to_id(category)
+    recipe.finished_item_id = item_name_to_id(finished_item)
+    recipe.material_id = item_name_to_id(material)
+    recipe.material_count = material_count
+
+    session.add(recipe)
+    session.commit()
+
+    
+def import_recipe_for_csv(path):
+    with open(path,"r",encoding="shift-jis") as f:
+        cin = csv.reader(f)
+        villains = [row for row in cin]
+        for row in villains:
+            add_recipe(row[1],row[2],row[3],row[4])
