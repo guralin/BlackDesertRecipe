22b1ec880204018abbdac0569fcfd85bce0a2d93
guralin <tellurium38@gmail.com>
2019-11-29 15:30:55 +0900

    initial_data.sqlを現在のデータベースに同期

--- RecipeApp/views.py
+++ RecipeApp/views.py
@@ -7,13 +7,12 @@ from RecipeApp import app
 from RecipeApp.models import recipe
 
 @app.route('/', methods=['POST'])
-def index():
+def show_recipe_material():
     request_body = request.json
     response = {}
     finished_item = request_body.get("finished_item")
     response["finished_item"] = finished_item
     response["material"] = recipe.show_matching_recipe(finished_item)
-
 
     return make_response(jsonify(response))
     
